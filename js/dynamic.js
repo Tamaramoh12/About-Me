c5d33c528b2d24c523adcedefa9ce0dd16dbeabe
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2020-09-12 18:53:49 +0300

    last touches

--- js/dynamic.js
+++ js/dynamic.js
@@ -3,23 +3,9 @@
 var sum = 0;
 
 // //the welcoming msg
-alert('Welcome to my personal web page, I am going now to ask you some questions in order to  find the common things' +
+alert('Welcome to my personal web page, I am going now to ask you some questions in order to  find the common things ' +
 'between me and you, so please answer using YES/NO or Y/N.');
 
-//  //Q1
-// var age = prompt('Is your age between 18 - 28 ?');
-// if (age.toUpperCase() == 'YES' || age.toUpperCase() == 'Y'){
-//     //console.log('Yeah, We are almost the same gneration.');
-//     alert ('Yeah, We are almost the same generation.')
-//     sum +=1;
-// } else if (age.toUpperCase() == 'NO' || age.toUpperCase() == 'N') {
-//     //console.log('Oh no, I wish we were the same age.');
-//     alert ('Oh no, I wish we were the same age.')
-// } else {
-//     //console.log('Put your answer please.');
-//     alert ('Put your answer please.')
-// }
-// console.log('The 1st question was \'Is your age between 18 - 28 ? and you answered\' ' + age);
 function q1(){
  var age = prompt('Is your age between 18 - 28 ?');
 if (age.toUpperCase() == 'YES' || age.toUpperCase() == 'Y'){
@@ -38,21 +24,6 @@ if (age.toUpperCase() == 'YES' || age.toUpperCase() == 'Y'){
 q1();
 
 
-// // //Q2
-// var volunteer = prompt('Do you like volunteering ?');
-// if (volunteer.toUpperCase() == 'YES' || volunteer.toUpperCase() == 'Y'){
-//     //console.log('Great, We both have a volunteer hobby.');
-//     alert ('Great, We both have a volunteer hobby.');
-//     sum +=1;
-// } else if (volunteer.toUpperCase() == 'NO' || volunteer.toUpperCase() == 'N') {
-//     //console.log('Oh no, I wish you like it.');
-//     alert ('Oh no, I wish you like it.')
-// } else {
-//     //console.log('Put your answer please.');
-//     alert ('Put your answer please.');
-// }
-// console.log('The 2nd question was \'Do you like volunteering ?\' and you answered '+ volunteer);
-
 function q2(){
     var volunteer = prompt('Do you like volunteering ?');
 if (volunteer.toUpperCase() == 'YES' || volunteer.toUpperCase() == 'Y'){
@@ -69,20 +40,7 @@ if (volunteer.toUpperCase() == 'YES' || volunteer.toUpperCase() == 'Y'){
 console.log('The 2nd question was \'Do you like volunteering ?\' and you answered '+ volunteer);}
 q2();
 
-// // //Q3
-// var major = prompt('Did you study something related to Information Technology ?');
-// if (major.toUpperCase() == 'YES' || major.toUpperCase() == 'Y'){
-//     //console.log('Wow! So we have the same majors.');
-//     alert ('Wow! So we have the same majors.')
-//     sum +=1;
-// } else if (major.toUpperCase() == 'NO' || major.toUpperCase() == 'N') {
-//     //console.log('Oh no, I wish that you are IT student or IT Hobbyist.');
-//     alert ('Oh no, I wish that you are an IT student or IT Hobbyist.')
-// } else {
-//     //console.log('Put your answer please.');
-//     alert ('Put your answer please.');
-// }
-// console.log('The 3rd question was\'Did you study something related to Information Technology ?\' and you answered '+ major);
+
 function q3(){
     var major = prompt('Did you study something related to Information Technology ?');
     if (major.toUpperCase() == 'YES' || major.toUpperCase() == 'Y'){
@@ -100,20 +58,7 @@ function q3(){
 }
 q3();
 
-// // //Q4
-// var drink = prompt('Do you love Nescafe ?');
-// if (drink.toUpperCase() == 'YES' || drink.toUpperCase() == 'Y'){
-//     //console.log('actually this make me happy, because I am Nescafe addicted.');
-//     alert ('actually this make me happy, because I am Nescafe addicted.')
-//     sum +=1;
-// } else if (drink.toUpperCase() == 'NO' || drink.toUpperCase() == 'N') {
-//     //console.log('Oh no, I wish that you love it.');
-//     alert ('Oh no, I wish that you love it.')
-// } else {
-//     //console.log('Put your answer please.');
-//     alert ('Put your answer please.');
-// }
-// console.log('The 4th question was\'Do you love Nescafe ?\' and you answered '+ drink);
+
 function Q4(){
     var drink = prompt('Do you love Nescafe ?');
 if (drink.toUpperCase() == 'YES' || drink.toUpperCase() == 'Y'){
@@ -131,20 +76,7 @@ console.log('The 4th question was\'Do you love Nescafe ?\' and you answered '+ d
 }
 Q4();
 
-// //Q5
-// var time = prompt('Are you a morning person ?');
-// if (time.toUpperCase() == 'YES' || time.toUpperCase() == 'Y'){
-//     //console.log('Wow, I really love early mornings\'.');
-//     alert ('Wow, I really love early mornings\'.')
-//     sum +=1;
-// } else if (time.toUpperCase() == 'NO' || time.toUpperCase() == 'N') {
-//     //console.log('Trust me, early mornings\' is better');
-//     alert ('Trust me, early mornings\' is better')
-// } else {
-//     //console.log('Put your answer please.');
-//     alert ('Put your answer please.');
-// }
-// console.log('The 5th question was\'Are you a morning person ?\' and you answered '+ time);
+
 function q5(){
     var time = prompt('Are you a morning person ?');
 if (time.toUpperCase() == 'YES' || time.toUpperCase() == 'Y'){
@@ -163,29 +95,6 @@ console.log('The 5th question was\'Are you a morning person ?\' and you answered
 q5();
 
 
-
-// var userName = prompt('Even if we have common characteristics or no, for sure you are most welcome. So, What is your name dear? ');
-// alert('Welcome ' + userName + ' ,wish you enjoy here.')
-
-// //Q6
-// //my num is 97
-// alert('lets play somthing.');
-// for (var counter = 1; counter <= 4; counter++){
-//     // var userNum(Number) = userNum;
-//     var userNum = prompt('Guess a number'); 
-//     if(userNum == 97){
-//         sum +=1;
-//         alert ('WOW, Perfect, You guess the right number.');
-//         break;
-//     } else if (userNum <= 90) {
-//         alert ('too low!!, try another number.');
-//     } else if (userNum >= 100) {
-//         alert ('too high!!, try another number.');
-//     } else if (userNum >= 95 && userNum <=99){
-//         alert ('Game over, the correct number was 97.');
-//     }   
-// }
-// console.log('The 6th question was about gussing number, and the correct answer is 97.');
 function q6(){
 
 alert('lets play somthing.');
@@ -209,24 +118,6 @@ console.log('The 6th question was about gussing number, and the correct answer i
 q6();
 
 
-// //Q7
-// var color = ['black','white','pink','blue','gray','green','yellow','purple']; 
-// var userColor = prompt('Ummm, can you guess my favorite color ?')
-// var counter =  1;
-// while( counter < 6 ){
-//     if (userColor == color[0] || userColor == color[2] || userColor == color[3] || userColor == color[5]) {
-//         alert ('your\'re brilliant, you got it, black and the light colors are the favorite colors to me');
-//         sum +=1;
-//         break;
-//     }
-//     else{
-//         alert('try another color.')
-//         prompt('Ummm, can you guess another color ?')
-//     }
-//     counter++;
-// } 
-// alert('The correct answer was: Black or pink or blue or gray');
-// console.log('The 7th question was about my favorite color, and the correct answer is black, pink, blue, and gray.');
 function q7(){
     var color = ['black','white','pink','blue','gray','green','yellow','purple']; 
 var userColor = prompt('Ummm, can you guess my favorite color ?')
@@ -250,4 +141,4 @@ q7();
 
 // //the result
 console.log('You got '+ sum + ' out of 7. ' + 'Thank you for your time.');
-// Haya's changes.
+
